813769bd374407bae122c07523da5f3ab5f0ff35
Bilal <affanchohan786@gmail.com>
2026-08-06 22:36:56 +0500

    write play_round ftn using basic if else

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let humanscore,computerscore=0;
 function get_computer_choice(){
     let choice=['Rock','Paper','Scissor']
     let pick= choice[Math.floor(Math.random()*choice.length)]
@@ -10,4 +11,39 @@ function get_human_choice(){
     console.log(choice[pick-1])
     return choice[pick-1];
 }
-get_human_choice();
+function play_round(computer_choice, human_choice){
+    if(computer_choice==human_choice){
+        console.log('Draw')
+    }
+    else if(computer_choice=='Rock'){
+        if(human_choice=='Paper'){
+            console.log('You Won');
+            console.log(`Score so far is : ${++humanscore} / ${computerscore}`)
+        }
+        else if(human_choice=='Scissor'){
+            console.log("Yahoo, i picked rock! looooser!!!");
+            console.log(`Score so far is : ${humanscore} / ${++computerscore}`)
+        }
+    }
+    else if (computer_choice=='Paper'){
+        if(human_choice=='Rock'){
+            console.log('Yahoo, i picked paper! looooser!!!');
+            console.log(`Score so far is : ${humanscore} / ${++computerscore}`)
+        }
+        else if(human_choice=='Scissor'){
+            console.log('Phewww..!You Won');
+            console.log(`Score so far is : ${++humanscore} / ${computerscore}`)
+        }
+    }
+    else if (computer_choice=='Scissor'){
+        if(human_choice=='Rock'){
+            console.log('Phewww..!You Won');
+            console.log(`Score so far is : ${++humanscore} / ${computerscore}`)
+        }
+        else if(human_choice=='Paper'){
+            console.log('Yahoo, i picked scissor! looooser!!!');
+            console.log(`Score so far is : ${humanscore} / ${++computerscore}`)
+        }
+    }   
+}
+
