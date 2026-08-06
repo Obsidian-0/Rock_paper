a19812d5937fb86c97ca6d641f2b2540ebb3e4f7
Bilal <affanchohan786@gmail.com>
2026-08-06 22:06:22 +0500

    write human side ftn

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function get_computet_choice(){
+function get_computer_choice(){
     let choice=['Rock','Paper','Scissor']
     let pick= choice[Math.floor(Math.random()*choice.length)]
     console.log(pick)
+    return pick;
 }
-get_computet_choice()
+function get_human_choice(){
+    let choice=['Rock','Paper','Scissor']
+    let pick = parseInt(prompt('Enter your choice: 1 for Rock, 2 for Paper, 3 for Scissor'))
+    console.log(choice[pick-1])
+    return choice[pick-1];
+}
+get_human_choice();
